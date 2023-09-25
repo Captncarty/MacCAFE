8be5ed5c9b01d25e78c3039afc2b630d76c0c82d
Kelvin <edithmukoro3@gmail.com>
2023-09-25 08:50:08 +0100

    adding more functionality to the database and admin_user

--- app/models/user.py
+++ app/models/user.py
@@ -9,12 +9,46 @@ class User(db.Model, UserMixin):
     created_at = db.Column(db.DateTime, nullable=False)
     expires_at = db.Column(db.DateTime, nullable=False)
     reference = db.Column(db.Integer, unique=True, nullable=False)
+    package = db.Column(db.String, nullable=False)
+    duration = db.Column(db.String, nullable=False)
+    price = db.Column(db.String, nullable=False)
+    speed = db.Column(db.String, nullable=False)
     is_admin = db.Column(db.Boolean, default=False)
 
-    def __init__(self, username, password, reference, is_admin):
-         self.username = username
-         self.password = password
-         self.created_at = datetime.utcnow()
-         self.expires_at = datetime.utcnow() + timedelta(days=1)
-         self.reference = reference
-         self.is_admin = is_admin
+
+    def __init__(self, username, password, reference, package, duration, price, is_admin):
+        self.username = username
+        self.password = password
+        self.created_at = datetime.utcnow()
+        #  self.expires_at = datetime.utcnow() + timedelta(days=1)
+        self.expires_at = self.calculate_expiration(duration)
+        self.reference = reference
+        self.package = package
+        self.duration = duration + " hours"
+        self.price = price + " Naira"
+        self.speed = self.calculate_speed(package)
+        self.is_admin = is_admin
+
+    def calculate_expiration(self, duration):
+        data_duration = int(duration)
+        if data_duration == 1:
+            return self.created_at + timedelta(days=1)
+        elif data_duration == 2:
+            return self.created_at + timedelta(days=1)
+        elif data_duration == 5:
+            return self.created_at + timedelta(days=2)
+        elif data_duration == 8:
+            return self.created_at + timedelta(days=3)
+        elif data_duration == 12:
+            return self.created_at + timedelta(days=3)
+        else:
+            return self.created_at + timedelta(days=7)
+    
+    def calculate_speed(self, package):
+        package_speed = package
+        if package_speed == "Lite":
+            return f'80mbps'
+        elif package_speed == "Fast":
+            return f'150mbps'
+        else:
+            return f'300mbps'
