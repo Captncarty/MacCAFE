3d2a4aff73fd2eab5484e0db6b4c1cad9eda0295
Kelvin <edithmukoro3@gmail.com>
2023-09-25 09:03:04 +0100

    blueprint files and routes to flask app

---
+++ app/views/blueprint.py
@@ -0,0 +1,260 @@
+import logging
+import requests
+from os import environ as env
+import requests
+from flask import Blueprint, render_template, jsonify, request, flash, redirect, url_for, abort
+from flask_login import login_user, current_user, login_required, logout_user
+from flask_login import LoginManager
+from app.models.gen_user import generate_username, generate_password
+from app.models.user import User
+from app import db, app, send_email, send_email_subscribe, get_paystack_transaction
+from datetime import datetime, timedelta
+
+
+secret_key = env.get('PAYSTACK_SECRET_KEY')
+public_key = env.get('PAYSTACK_PUBLIC_KEY')
+
+"""
+log user
+"""
+
+logging.basicConfig(filename='app.log', level=logging.INFO, format='%(asctime)s - %(levelname)s - %(message)s')
+
+
+login_manager = LoginManager(app)
+login_manager.login_view = "my_blueprint.login"
+@login_manager.user_loader
+
+def load_user(user_id):
+    """AI is creating summary for load_user
+
+    Args:
+        user_id ([type]): [description]
+
+    Returns:
+        [type]: [description]
+    """
+    # Load a user object from your database based on user_id
+    return User.query.get(int(user_id))
+
+# def delete_expired_users():
+#     """ delete users that have hit the expiration time """
+#     try:
+#         expiration_time = datetime.utcnow()
+#         expired_users = User.query.filter(User.created_at < expiration_time).all()
+
+#         with app.app_context():
+#             with db.session.begin():
+#                 for user in expired_users:
+#                     db.session.delete(user)
+                
+#                 # Commit the transaction after deleting all users
+#                 db.session.commit()
+#                 app.logger.info('Expired users deleted successfully')
+
+#     except Exception as e:
+#         app.logger.error(f'An error occurred while deleting expired users: {e}')
+
+#     scheduler.add_job(delete_expired_users, 'interval', hours=24)
+
+
+my_blueprint = Blueprint('my_blueprint', __name__)
+
+@my_blueprint.route('/')
+def index():
+    if current_user.is_authenticated:
+        return redirect(url_for('my_blueprint.dashboard'))
+    return render_template('landing.html', paystack_public_key=public_key)
+
+
+@my_blueprint.route('/redirect_url', methods=['GET', 'POST'])
+def redirect_url():
+    return redirect("http://www.google.com")
+
+
+@my_blueprint.route('/stats', strict_slashes=False)
+def stats():
+    return jsonify('working alright') 
+
+
+@app.route('/verify_transaction', methods=['POST'])
+def verify_transaction():
+    data = request.get_json()
+    reference = data.get('reference')
+    email = data.get('email')
+    package = data.get('package')
+    duration = data.get('duration')
+    price = data.get('price')
+    # user = User.query.get(reference)
+
+    # if user.reference == reference:
+    #     abort(401)
+    #     app.logger.info(f'Reference no: {reference}, Already verified!')
+    # Call the get_paystack_transaction function
+    # Retrieve data from session storage
+    # package = request.cookies.get('selectedPackage')
+    # duration = request.cookies.get('selectedDuration')
+    # price = request.cookies.get('selectedPrice')
+    result = get_paystack_transaction(reference, secret_key)
+    app.logger.info(f'Verifying transaction with reference: {reference}')
+
+    if result is not None:
+        # Transaction was successfully verified
+        
+        username = generate_username()
+        password = generate_password()
+
+        user = User(username=username, password=password, reference=reference, package=package, duration=duration, price=price, is_admin=False)
+        db.session.add(user)
+        db.session.commit()
+        send_email(email, username, password, duration, package)
+        app.logger.info(f'Transaction verified! User ID: {user.id} Username: {username} Package: {package} Duration: {duration}')
+        return jsonify({'result': True})
+    else:
+        # Transaction verification failed
+        return jsonify({'result': False})
+
+
+@my_blueprint.route('/create_user', methods=['GET', 'POST'])
+def create_user():
+    if request.method == 'POST':
+        email = request.form.get('email')
+    
+        username = generate_username()
+        password = generate_password()
+        reference = 830031789
+
+        user = User(username=username, password=password, reference=reference, is_admin=False)
+        db.session.add(user)
+        db.session.commit()
+        
+        send_email(email, username, password)
+
+        flash('User created successfully. Check your email for credentials.', 'success')
+        return redirect(url_for('my_blueprint.login'))
+        
+        #return f'New User Created<br>Username: {username}<br>Password: {password}'
+    #return render_template('create_user.html')
+    return redirect(url_for('my_blueprint.index'))
+
+
+@my_blueprint.route('/login', methods=['GET', 'POST'])
+def login():
+    if request.method == 'POST':
+        username = request.form.get('username')
+        password = request.form.get('password')
+
+        user = User.query.filter_by(username=username).first()
+        
+        if user.expires_at < datetime.utcnow():
+            if user.is_admin:
+                pass
+            else:
+                app.logger.info(f'{username} has expired and deleted from the database')
+                db.session.delete(user)
+                db.session.commit()
+            
+                flash(f'{username} has expired')
+                return redirect(url_for('my_blueprint.index'))
+        
+        if user and user.password == password:
+            login_user(user)
+            if current_user.is_admin:
+                app.logger.info('logged in Admin')
+                return redirect(url_for('admin_dashboard'))
+                #return redirect("http://www.google.com")
+            else:
+                logging.info('User logged in: {username}'.format(username=username))
+                
+                return redirect(url_for('my_blueprint.redirect_url'))
+        elif user:
+            logging.info('Invalid password: {username}'.format(username=username))
+            flash('Invalid password', 'try again')
+            return redirect(url_for('my_blueprint.index'))
+        else:
+            flash('Login failed. Check credentials', 'try again')
+    return redirect(url_for('my_blueprint.index'))
+
+
+@my_blueprint.route('/logout')
+def logout():
+    if current_user:
+        logging.info('User logged out: %s', current_user.username)
+        logout_user()
+        flash('Logout successful', 'success')
+        return redirect(url_for('my_blueprint.index'))
+    flash("You're not logged in", 'fail')
+    return render_template("welcome.html")
+
+
+# @my_blueprint.route('/logout_inactive')
+# @login_required
+# def logout_inactive():
+#     # Check if the user has been inactive for a specified period (e.g., 30 minutes)
+#     inactivity_threshold = timedelta(seconds=30)
+#     if datetime.utcnow() - current_user.last_activity >= inactivity_threshold:
+#         logout_user()
+#         flash('You have been logged out due to inactivity.', 'info')
+#         return redirect(url_for('my_blueprint.login'))
+
+#     return redirect(url_for('my_blueprint.dashboard'))
+
+
+# @my_blueprint.route('/api/active_users', methods=['GET'])
+# @login_required
+# def view_active_users():
+#     if current_user.is_admin:  # Check if the user is an admin (customize this condition)
+#         # Define an inactivity threshold (e.g., 30 minutes)
+#         inactivity_threshold = timedelta(minutes=30)
+#         active_users = [user for user in User.query.all() if (datetime.utcnow() - user.last_activity) <= inactivity_threshold]
+#         active_user_list = [{'id': user.id, 'username': user.username} for user in active_users]
+#         return jsonify(active_users=active_user_list)
+#     else:
+#         return jsonify(message='Permission denied'), 403
+
+
+@my_blueprint.route('/dashboard')
+@login_required
+def dashboard():
+    if current_user:
+        user = current_user.username
+        return render_template("welcome.html", user=user)
+
+
+@my_blueprint.route('/delete_user/<int:user_id>', methods=['POST'])
+def delete_user(user_id):
+    # Ensure that only the logged-in user can delete their own account
+    # if current_user.id != user_id:
+    #     flash("You don't have permission to delete this user.", 'danger')
+    #     return redirect(url_for('my_blueprint.dashboard'))
+
+    # Delete the user from the database
+    user = User.query.get(user_id)
+    if user:
+        db.session.delete(user)
+        db.session.commit()
+        flash('User deleted successfully.', 'success')
+
+    return redirect(url_for('my_blueprint.logout'))
+
+# @my_blueprint.route('/api/users', methods=['GET'])
+# def view_users():
+#     #if current_user.is_admin:
+#     users = User.query.all()
+#     if users:
+#         user_list = [{'id': user.id, 'username': user.username} for user in users]
+#         return jsonify(users=user_list)
+#     return jsonify({'message': 'You are not authorized to perform that function.'}), 403
+
+
+@my_blueprint.route('/subscribe', methods=['GET', 'POST'])
+def subscribe():
+    if request.method == 'POST':
+        email = request.form.get('email')
+        
+        send_email_subscribe(email)
+
+        flash('Thank you for subscribing!', 'success')
+        app.logger.info('New subscriber: {email}'.format(email=email))
+        return redirect(url_for('my_blueprint.index'))
+    return render_template('blog.html')
---
+++ app/views/view.py
@@ -0,0 +1,196 @@
+"""
+setting up api calls and routes for the app
+"""
+import requests
+import random
+from os import environ as env
+from flask import Blueprint, render_template, jsonify, request, flash, redirect, url_for, abort
+from flask_login import login_user, current_user, login_required, logout_user
+from flask_login import LoginManager
+from app.models.gen_user import generate_username, generate_password
+from app.models.user import User
+from app import db, app, send_email, get_paystack_transaction
+from datetime import datetime, timedelta
+
+# login_manager = LoginManager(app)
+# login_manager.login_view = "my_blueprint.login"
+# @login_manager.user_loader
+
+secret_key = env.get('PAYSTACK_SECRET_KEY')
+
+my_views = Blueprint('view', __name__)
+
+@app.route('/st')
+def run():
+    return jsonify('worked')
+
+
+@app.route('/api/users', methods=['GET'])
+@login_required
+def view_users():
+    if not current_user.is_admin:
+        abort(403)
+    users = User.query.all()
+    if users:
+        user_list = [{
+            'id': user.id,
+            'username': user.username,
+            'password': user.password,
+            'created_at': user.created_at,
+            'expires_at': user.expires_at,
+            'reference': user.reference,
+            'price': user.price,
+            'speed': user.speed,
+            'package': user.package,
+            'duration': user.duration
+        } for user in users]
+        return jsonify(users=user_list)
+    return jsonify({'message': 'You are not authorized to perform that function.'}), 403
+
+
+@app.route('/api/users/<int:user_id>', methods=['GET'])
+@login_required
+def users_by_id(user_id):
+    if not current_user.is_admin:
+        abort(403)
+    user = User.query.get(user_id)
+    if user:
+        return jsonify({'id': user.id, 'username': user.username, 'password': user.password, 'reference': user.reference})
+    return jsonify({'message': 'User not available.'}), 403
+
+
+@app.route('/api/create_user', methods=['GET', 'POST'])
+@login_required
+def create_user():
+    if current_user.is_admin:
+        username = generate_username()
+        password = generate_password()
+        reference = random.randint(100000000, 999999999)
+        package = "Lite"
+        duration = "2"
+        price = "5"
+
+
+        if username and password:
+            user = User(username=username, password=password, reference=reference, price=price, package=package, duration=duration, is_admin=False)
+            db.session.add(user)
+            db.session.commit()
+            app.logger.info(f'New User: user_id: {user.id} username: {user.username} reference no: {user.reference}, package: {user.package}, duration: {user.duration}')
+            return jsonify({'id': user.id, 'username': user.username, 'password': user.password, 'reference': user.reference, 'package': user.package, 'duration': user.duration})
+        return jsonify({'message': 'check details'}), 403
+    return abort(401)
+
+
+@app.route('/admin_dashboard')
+@login_required
+def admin_dashboard():
+    if current_user.is_admin:
+        # Only allow admin users to access the admin dashboard
+        return render_template('admin_dashboard.html')
+    else:
+        return 'Permission denied', 403
+
+@app.route('/user/<int:user_id>/delete', methods=['DELETE'])
+@login_required
+def delete_user(user_id):
+    if not current_user.is_admin:
+        abort(403)  # Forbidden for non-admin users
+    user_to_delete = User.query.get(user_id)
+    if not user_to_delete:
+        return jsonify({'message': 'User not found'}), 404
+    db.session.delete(user_to_delete)
+    db.session.commit()
+    return jsonify({'message': 'User deleted successfully'})
+
+
+
+@app.route('/api/user/search', methods=['GET', 'POST'])
+@login_required
+def search():
+    if request.method == 'POST':
+        if not current_user.is_admin:
+            abort(403)
+        username = request.form.get('username')
+
+        user_details = User.query.filter_by(username=username).first()
+        if user_details:
+            return render_template('admin_dashboard.html', user_details=user_details.username)
+        flash('User not found', 'failed')
+    return redirect(url_for('admin_dashboard'))
+
+
+@app.route('/api/search/<string:username>', methods=['GET'])
+@login_required
+def users_by(username):
+    if not current_user.is_admin:
+        abort(403)
+    user = User.query.filter_by(username=username).first()
+    if user:
+        return render_template('admin_dashboard.html', user_details=user)
+    return jsonify({'message': 'User not available.'}), 403
+
+@app.route('/transactions/<string:reference>', methods=['GET'])
+@login_required
+def get_transaction_by_reference(reference):
+    if not current_user.is_admin:
+        abort(403)
+    # Verify the transaction using the verify_transaction function
+    secret_key = env.get('PAYSTACK_SECRET_KEY')
+    verify_url = f'https://api.paystack.co/transaction/verify/{reference}'
+    headers = {
+        'Authorization': f'Bearer {secret_key}',
+        'Cache-Control': 'no-cache',
+    }
+
+    response = requests.get(verify_url, headers=headers)
+
+    if response.status_code == 200:
+        transaction_data = response.json()
+        # Process the transaction data as needed
+        return jsonify(transaction_data)
+    else:
+        return jsonify({'error': 'Transaction verification failed'}), 400
+    
+
+@app.route('/transactions', methods=['GET'])
+@login_required
+def get_all_transactions():
+    if not current_user.is_admin:
+        abort(403)
+    secret_key = env.get('PAYSTACK_SECRET_KEY')
+    url = "https://api.paystack.co/transaction"
+
+    headers = {
+        "Authorization": f"Bearer {secret_key}",
+        "Cache-Control": "no-cache",
+    }
+
+    response = requests.get(url, headers=headers)
+
+    if response.status_code == 200:
+        transaction_data = response.json()
+        # Process the transaction data as needed
+        return jsonify(transaction_data)
+    else:
+        return jsonify({'error': 'Transaction verification failed'}), 400\
+            
+@app.route('/blog')
+def blog_page():
+    return render_template('blog.html')
+
+@app.route('/api/verify', methods=['POST'])
+def verify():
+    if not current_user.is_admin:
+        abort(403)
+    data = request.get_json()
+    reference = data.get('reference')
+    
+    result = get_paystack_transaction(reference, secret_key)
+    app.logger.info(f'Verifying transaction with reference: {reference}')
+
+    if result is not None:
+        app.logger.info(f'Transaction verified!')
+        return jsonify({'result': True})
+    else:
+        # Transaction verification failed
+        return jsonify({'result': False})
